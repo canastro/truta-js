3879a2e6c41e8a80c52502aa4dac0cfca06f5a1d
Ricardo Canastro <ricardo.canastro@blip.pt>
2015-06-07 17:31:25 +0100

    Added bootstrap function to truta

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
 import { $, Store, Router, Truta } from './lib/index';
 
-function loadImport (path, resolve, reject) {
-    var link = document.createElement('link');
-
-    function handleLoad(e) {
-        console.log('Loaded import: ' + e.target.href);
-        resolve(link);
-    }
-    function handleError(e) {
-        console.log('Error loading import: ' + e.target.href);
-        reject();
-    }
-
-
-    link.rel = 'import';
-    link.href = path;
-    link.onload = handleLoad;
-    link.onerror = handleError;
-    document.head.appendChild(link);
-}
-
-var urls = [
+Truta.bootstrap([
     '/views/components/profile-form/index.html',
     '/views/components/header/index.html'
-];
-
-var promises = [];
-
-urls.forEach(function (url) {
-    promises.push(new Promise(function (resolve, reject) {
-        loadImport(url, resolve, reject)
-    }))
-});
-
-Promise.all(promises).then(init);
-
-
-// promise.then(init);
-
-
-function init (link) {
-
-    var data = {
-        name: 'teste',
-        last_name: 'teste'
-    };
+]).then(function () {
 
     Truta
         .component('profile-form', function () {
@@ -110,4 +69,4 @@ function init (link) {
         debugger;
         console.log(Store);
     }, false);
-}
+});
